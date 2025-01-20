7109b44384db720be4a7a745e5ac8e0af13c699a
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2025-01-20 17:45:38 +0530

    feat: add new service

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ app.get('/products', (req, res)=>{
   res.json({message: 'Hare Krishna', status: 200})
 })
 
+app.get('/services', (req, res)=>{
+  res.json({message: 'Service Running Successfully', status: 200})
+})
+
 app.listen(PORT, ()=>{
   console.log(`app is listening to port ${PORT}`)
 })
